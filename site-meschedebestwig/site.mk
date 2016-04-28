961f9efbd95b1fe11ce9399a4ef011c60c80a802
Lars Bußmann <github@kill-you.net>
2016-04-28 20:40:26 +0200

    all: switch to l2tp, new gluon v2016.1.3, extend subnet to /20

--- site-meschedebestwig/site.mk
+++ site-meschedebestwig/site.mk
@@ -5,9 +5,9 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
+	gluon-config-mode-tunneldigger \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-neighbour-info \
@@ -17,16 +17,18 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-portconfig \
 	gluon-luci-wifi-config \
 	gluon-next-node \
-	gluon-mesh-vpn-fastd \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-migrate-vpn \
 	gluon-radvd \
 	gluon-respondd \
 	gluon-status-page \
 	gluon-setup-mode \
+	gluon-tunneldigger-watchdog \
 	iwinfo \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.8.2-meschedebestwig-stable$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.8.4-meschedebestwig-$(shell date '+%y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
